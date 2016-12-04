1d87a0fed54ef6c4a2ec365090f5267385de4f0b
Tom Barton <grez.cz@gmail.com>
2016-12-05 00:03:09 +0100

    Added caching

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -6,17 +6,25 @@ use AppBundle\Libs\Product\ProductRepository;
 use AppBundle\Libs\VisitLogger\Logger;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Cache\Adapter\AbstractAdapter;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ProductController extends Controller
 {
-
 	/**
 	 * @Route("/detail/{id}", name="detail_show")
 	 */
-	public function detailAction($id)
+	public function detailAction(int $id)
 	{
-		$product = $this->getProductRepository()->findById($id);
+		$cachedProduct = $this->getCache()->getItem('product-detail' . $id);
+		if (!$cachedProduct->isHit()) {
+			$product = $this->getProductRepository()->findById($id);
+			$cachedProduct->expiresAfter(60);
+			$cachedProduct->set($product);
+			$this->getCache()->save($cachedProduct);
+		} else {
+			$product = $cachedProduct->get();
+		}
 		$this->getLogger()->logVisit('product-detail', $id);
 
 		$response = new JsonResponse();
@@ -38,5 +46,13 @@ class ProductController extends Controller
 	private function getProductRepository()
 	{
 		return $this->get('app.productRepository');
+	}
+
+	/**
+	 * @return AbstractAdapter
+	 */
+	private function getCache()
+	{
+		return $this->get('cache.app');
 	}
 }
